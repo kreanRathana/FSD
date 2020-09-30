e60639a9e06e5655956185d8ea44b0d37991f38c
DaroLab <labdaro18@kit.edu.kh>
2020-09-30 15:03:48 +0700

    add new

--- client/src/components/CategoriesPlace.vue
+++ client/src/components/CategoriesPlace.vue
@@ -18,15 +18,36 @@
     <v-main>
       <v-container>
         <v-row>
-          <v-col
-            cols="12"
-          >
-            <v-card height="400">
-                <v-row>
-                  <v-col class="col s1">Hel</v-col>
-                  <v-col class="col s4">Daro</v-col>
-                  <v-col class="col s6">Byge</v-col>
-                </v-row>
+          <v-col cols="12">
+            <v-card height="300">
+                  <v-row no-gutters>
+                    <!-- image -->
+                     <v-col cols="4">
+                        <v-card
+                           class="pa-2">
+                              <v-img
+                                :src="require('../assets/Angkor Wat.png')"
+                                height="250"
+                                width="400"
+                                class="grey darken-4">
+                            </v-img>
+                      </v-card>
+                      </v-col>
+                <v-col cols="6">
+                  <v-container
+                    class="pa-2"
+                    
+                  >
+                    <h1>Angkor Wat template</h1>
+                    <div>
+                      <p style="color:gray;font-family: 'Lucida Console', Courier, monospace;">Siem Reap, Cambodia</p>
+                    </div>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aliquam commodi quas consequuntur 
+                    eligendi? Facilis illo odio sed sapiente aspernatur soluta,Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aliquam commodi quas consequuntur 
+                    eligendi? Facilis illo odio sed sapiente aspernatur soluta</p>
+                  </v-container>
+                </v-col>
+              </v-row>
             </v-card>
           </v-col>
         </v-row>
