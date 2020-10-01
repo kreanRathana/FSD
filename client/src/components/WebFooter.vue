338f78efcd6691b55fdababf13825a10170ee4bc
Saonan <thysaonan007@gmail.com>
2020-10-01 12:23:09 +0700

    WebFooter.vue is editted

--- client/src/components/WebFooter.vue
+++ client/src/components/WebFooter.vue
@@ -1,75 +1,58 @@
 <template>
-  <footer class="page-footer">
-    <div class="container">
-      <div class="row">
-        <div class="col s4">
-          <a href="#"><img src="../assets/nvna_logo.jpg" style="border-radius: 50%;" width="70" height="70" alt="Project Logo" /></a>
-          <h5 class="white-text">Contact</h5>
-          <p>+855 123 456 789</p>
-          <p>tvna19@kit.edu.kh</p>
-          <p></p>
-        </div>
-
-        <div class="col s4">
-          <h5 class="white-text">Information</h5>
-          <ul>
-            <li><a class="grey-text text-lighten-3" href="#!">About</a></li>
-            <li><a class="grey-text text-lighten-3" href="#!">FAQ</a></li>
-            <li>
-              <a class="grey-text text-lighten-3" href="#!">Term&Condition</a>
-            </li>
-            <li>
-              <a class="grey-text text-lighten-3" href="#!">Privacy Policy</a>
-            </li>
-          </ul>
-        </div>
-            
-        <div class="col s4">
-          <div class="row">
-            <br />
-            <p style="font-size: 16px">Subscribe for more info</p>
-            <form class="col s12">
-              <div class="row">
-                <div class="input-field col s12">
-                  <input
-                    id="email"
-                    type="email"
-                    placeholder="Enter your Email"
-                  />
-                  <input
-                    id="submit"
-                    type="submit"
-                    value="Subscibe"
-                    class="btn waves-effect waves-light"
-                  />
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="footer-copyright">
-      <div class="container">
-        <center><p>© 2020 Copyright TvNa.com.kh</p></center>
-      </div>
-    </div>
-  </footer>
+  <v-footer class="green lighten-1 white--text" padless>
+    <v-row class="pt-1" no-gutters>
+      <v-col class="pl-10" cols="12" sm="4">
+        <a href="#"
+          ><img
+            src="../assets/nvna_logo.jpg"
+            style="border-radius: 50%"
+            width="70"
+            height="70"
+            alt="Project Logo"
+        /></a>
+        <h3 style="padding: 10px">Contact</h3>
+        <p>+855 123 456 789<br />tvna19@kit.edu.kh</p>
+      </v-col>
+      <v-col cols="12" sm="4">
+        <h3 style="padding: 10px">Information</h3>
+        <ul>
+          <li><a href="#!">About</a></li>
+          <li><a href="#!">FAQ</a></li>
+          <li>
+            <a href="#!">Term&Condition</a>
+          </li>
+          <li>
+            <a href="#!">Privacy Policy</a>
+          </li>
+        </ul>
+      </v-col>
+      <v-col class="pr-10" cols="12" sm="4">
+        <h4 style="padding-top: 15px">Subscribe For More Information</h4>
+        <v-text-field
+          class="pt-5"
+          label="Email"
+          placeholder="Enter Your Email"
+          outlined
+        ></v-text-field>
+        <v-btn rounded color="primary white--text pa-5"> Subscribe </v-btn>
+        <p></p>
+      </v-col>
+      <v-col class="green py-4 text-center white--text" cols="12">
+        {{ "CopyRight@"+ new Date().getFullYear() }} — <strong>TvNa.com.kh</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
 </template>
-
 <script>
-export default {
-  
-}
+export default {};
 </script>
-<style>
-.page-footer {
-  background-color: #00c689;
+<style scoped>
+ul li {
+  list-style: none;
+  padding-bottom: 10px;
 }
-#email {
-  border-radius: 10px;
-  border: 2px solid;
-  padding-left: 10px;
+ul li a {
   text-decoration: none;
+  color: white;
 }
 </style>
